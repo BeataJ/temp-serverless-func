1ffc7b44414faea660a170ac84d22863583159d7
beata <beata@emc2wave.com>
2021-02-07 23:06:57 -0800

    cors error fix

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,6 +2,9 @@ const items = require('../assets/data');
 
 exports.handler = async (event, contex, cb) => {
   return {
+    headers: {
+      'Access-Control-Allow-Orgin': '*',
+    },
     statusCode: 200,
     body: JSON.stringify(items),
   };
