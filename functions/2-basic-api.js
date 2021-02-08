c9c79d9ec82b0311197830327a386276215b20b1
beata <beata@emc2wave.com>
2021-02-07 23:08:33 -0800

    correct name error

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -3,7 +3,7 @@ const items = require('../assets/data');
 exports.handler = async (event, contex, cb) => {
   return {
     headers: {
-      'Access-Control-Allow-Orgin': '*',
+      'Access-Control-Allow-Origin': '*',
     },
     statusCode: 200,
     body: JSON.stringify(items),
