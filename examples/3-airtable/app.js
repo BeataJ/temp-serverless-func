3dc4fefa1281630186eee6cb453072f596e63d8b
beata <beata@emc2wave.com>
2021-02-08 21:30:55 -0800

    work airtable.js app.js

--- examples/3-airtable/app.js
+++ examples/3-airtable/app.js
@@ -1,1 +1,12 @@
-const result = document.querySelector('.result')
+const result = document.querySelector('.result');
+
+const fetchProducts = async () => {
+  const { data } = await axios.get('/api/3-airtable');
+  try {
+    console.log(data);
+  } catch (error) {
+    result.innerHTML = '<h4>There was an error</h4>';
+  }
+};
+
+fetchProducts();
--- functions/3-airtable.js
+++ functions/3-airtable.js
@@ -1,9 +1,27 @@
+require('dotenv').config();
+const Airtable = require('airtable-node');
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base('app4uYq3dbjHy3tDx')
+  .table('products');
+
 exports.handler = async (event, contex, cb) => {
-  return {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-    statusCode: 200,
-    body: 'Aittable Example',
-  };
+  try {
+    const { records } = await airtable.list();
+    const products = records.map((product) => {
+      const { id } = product;
+      const { name, image, price } = product.fields;
+      const url = image[0].url;
+      return { id, name, url, price };
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(products),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: 'Server Error',
+    };
+  }
 };
